95e89d7454aad1465b39aaab81307afd7dc215fc
daniel3g <silvadaniel2006@yahoo.com.br>
2026-04-02 09:26:22 -0300

    iniciamos com arrays

---
+++ app.js
@@ -0,0 +1,18 @@
+//array, vetor
+//push() insere elemento no final
+//unshift() insere elemento no início
+//pop() elimina elemento no final
+//length mostra a quantidade de elementos do array
+const nomes = ["Daniel", "Ana", "Sophia", "Geovana", "Enzo"]
+console.log(nomes)
+nomes.pop()
+nomes.unshift("Lara", "Eduardo", "Igor", "Luis", "Nalu")
+console.log(nomes)
+console.log(nomes.length)
+
+
+const notas = [8, 7, 9, 5, 7, 1, 5, 8, 9, 4]
+
+//Desafio, crie um programa usando um array notas com 10 notas e calcule a média delas
+
+ 
